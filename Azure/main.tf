46d9536b7917575523afca2e46f3a1633c250cb0
IngridOnline <ingrid.finnoy@soprasteria.com>
2021-08-11 16:06:07 +0200

    added virtual networks

--- Azure/main.tf
+++ Azure/main.tf
@@ -2,3 +2,24 @@ provider "azurerm" {
   features {}
 }
 
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "virtualNetwork1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_virtual_network" "example2" {
+  name                = "virtualNetwork2"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+
+
+}
+
