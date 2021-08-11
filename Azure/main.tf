bb6d379bee89ad71642b4a084dff005310c0b0cb
IngridOnline <ingrid.finnoy@soprasteria.com>
2021-08-11 16:22:20 +0200

    added attempt for network peering

--- Azure/main.tf
+++ Azure/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "example1" {
   name                = "virtualNetwork1"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
@@ -21,5 +21,19 @@ resource "azurerm_virtual_network" "example2" {
   address_space       = ["10.1.0.0/16"]
 
 
+
+}
+resource "azurerm_virtual_network_peering" "example-1" {
+  name                      = "peer1to2"
+  resource_group_name       = azurerm_resource_group.example.name
+  virtual_network_name      = azurerm_virtual_network.example1.name
+  remote_virtual_network_id = azurerm_virtual_network.example2.id
+}
+
+resource "azurerm_virtual_network_peering" "example-2" {
+  name                      = "peer2to1"
+  resource_group_name       = azurerm_resource_group.example.name
+  virtual_network_name      = azurerm_virtual_network.example2.name
+  remote_virtual_network_id = azurerm_virtual_network.example1.id
 }
 
