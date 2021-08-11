e987874077877d20615fb88c0ef349cd4a1e73f7
IngridOnline <ingrid.finnoy@soprasteria.com>
2021-08-11 16:08:04 +0200

    changed network-adress

--- Azure/main.tf
+++ Azure/main.tf
@@ -18,7 +18,7 @@ resource "azurerm_virtual_network" "example2" {
   name                = "virtualNetwork2"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.1.0.0/16"]
 
 
 }
