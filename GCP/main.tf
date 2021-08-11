9a888864df58a6cac0cf0eefe307950cb5b54db6
IngridOnline <ingrid.finnoy@soprasteria.com>
2021-08-11 15:48:22 +0200

    formatting

--- GCP/main.tf
+++ GCP/main.tf
@@ -1,5 +1,5 @@
 provider "google" {
- project = "devOps21-green"
- region = "europe-north1"
- zone = "europe-north1-a"
+  project = "devOps21-green"
+  region  = "europe-north1"
+  zone    = "europe-north1-a"
 }
