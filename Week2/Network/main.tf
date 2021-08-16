cc9b0eebf2849755449bb4b64d8c5ae6cdc5ccbf
songluu1 <song.luu@soprasteria.com>
2021-08-16 12:58:16 +0200

    Added VM

--- Week2/Network/main.tf
+++ Week2/Network/main.tf
@@ -51,3 +51,31 @@ resource "azurerm_network_interface" "Sub2-interface" {
     private_ip_address_allocation = "Dynamic"
   }
 }
+
+resource "azurerm_linux_virtual_machine" "VM1" {
+  name                = "example-VM1"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.Sub1-interface.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("id_rsa.pub")
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+}
